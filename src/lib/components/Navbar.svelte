bbddf143eb95d6acc1b0f15d92f1f12cc44caaaf
Sadeem Sajid <sadeemsajid.25@gmail.com>
2024-06-26 19:14:22 +0500

    feat: font sizes + styles

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,10 +2,12 @@
 
     // TODO: Theme persistence
     // TODO: OS theme preference
+    // TODO: Menu drawer
+    // TODO: Home nav
 
 </script>
 
-<div class="flex justify-between px-12 md:px-24 pt-16">
+<div class="flex justify-between px-4 md:px-24 md:pt-16 pt-8 pb-24">
     <div class="flex gap-4">
         <button class="btn btn-circle btn-sm btn-secondary"></button>
         <button class="btn btn-circle btn-sm btn-accent"></button>
---
+++ src/routes/(home)/+page.svelte
@@ -0,0 +1,4 @@
+
+<div class="border border-red-500 md:mx-48 mx-4">
+    <h1 class="font-bitter">:3 hehe</h1>
+</div>
