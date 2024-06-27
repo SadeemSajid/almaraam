9be6999c2493f5f167f758253194c933e254d840
Sadeem Sajid <sadeemsajid.25@gmail.com>
2024-06-27 14:13:16 +0500

    feat: navbar w/o drawer

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,16 +1,22 @@
-<script>
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    // OS Theme Preference
+    let darkMode = false;
+    onMount(() => {
+        // TODO: have system theme preference react to change as well (it does for me)
+        darkMode = window.matchMedia('prefers-color-scheme: dark').matches;
+    });
 
     // TODO: Theme persistence
-    // TODO: OS theme preference
     // TODO: Menu drawer
-    // TODO: Home nav
 
 </script>
 
 <div class="flex justify-between px-4 md:px-24 md:pt-16 pt-8 pb-24">
     <div class="flex gap-4">
         <button class="btn btn-circle btn-sm btn-secondary"></button>
-        <button class="btn btn-circle btn-sm btn-accent"></button>
+        <a href="/"><button class="btn btn-circle btn-sm btn-accent"></button></a>
     </div>
-    <input type="checkbox" checked={true} value="light" class="theme-btn" />
+    <input type="checkbox" checked={darkMode} value="light" class="theme-btn" on:click={() => darkMode = false}/>
 </div>
