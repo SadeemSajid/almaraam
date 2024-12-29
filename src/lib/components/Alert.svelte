4a709828207aeab34072abcf609984012c466809
Sadeem Sajid <sadeemsajid.25@gmail.com>
2024-12-29 10:49:10 +0500

    feat: alert component

---
+++ src/lib/components/Alert.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import CrossIcon from '~icons/material-symbols-light/cancel-rounded';
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	const closeAlert = () => {
+		dispatch('close');
+	};
+
+	export let message: string = '';
+	export let error: boolean = false;
+</script>
+
+<div
+	class="flex w-screen h-screen absolute left-0 top-0 bg-primary bg-opacity-50 backdrop-blur-sm justify-center"
+>
+	<div
+		role="alert"
+		class="alert w-fit rounded-none bg-{error
+			? 'error'
+			: 'primary'} h-28 flex justify-between mt-40 border-0"
+	>
+		<p>{message}.</p>
+		<button class="btn m-0 p-0 border-0" on:click={() => closeAlert()}>
+			<CrossIcon class="text-secondary w-12 h-12" />
+		</button>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,34 @@
 <script lang="ts">
 	import ArticleHome from '$lib/components/ArticleHome.svelte';
+	import Alert from '$lib/components/Alert.svelte';
+
 	import type { ArticleModelType } from '$lib/types/types';
 	import { onMount } from 'svelte';
 
+	// Articles fetched from DB
 	let articles: Array<ArticleModelType>;
+
+	// Controls displaying of alert
+	let alertController = false;
 
 	onMount(async () => {
 		// Fetches top 10 articles from the DB
 		const res = await fetch('/api/article/fetch/home');
-		// TODO: Show UI indication for this like a Toast maybe
 		if (!res.ok) {
-			console.log('Problem Fetching Articles');
+			alertController = true;
 		} else {
 			articles = (await res.json()).data;
 		}
 	});
 </script>
+
+{#if alertController}
+	<Alert
+		message="Articles could not be fetched. Please refresh the page"
+		error={true}
+		on:close={() => (alertController = false)}
+	/>
+{/if}
 
 <div class="mx-48 flex flex-col gap-8">
 	{#if articles}
